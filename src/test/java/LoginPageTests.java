97fd2de63065fae9c3658bb08119eb904016bf6f
Benjamin Hill <bennathill@benjamins-mbp.mynetworksettings.com>
2024-11-16 11:27:12 -0800

    Create Login page POM and use in LoginPageTests

--- src/test/java/LoginPageTests.java
+++ src/test/java/LoginPageTests.java
@@ -1,4 +1,4 @@
-import org.assertj.core.api.Assertions;
+import com.selenium.saucedemo.pages.LoginPage;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -7,14 +7,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SauceDemoTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LoginPageTests {
     private static WebDriver driver;
+    private static LoginPage loginPage;
 
     @BeforeClass
     public static void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
+
+        loginPage = new LoginPage(driver);
     }
 
     @AfterClass
@@ -23,18 +28,12 @@ public class SauceDemoTests {
     }
 
     @Test
-    public void testLogin() throws InterruptedException {
-        Thread.sleep(2000);
-
-        WebElement usernameField = driver.findElement(By.id("user-name"));
-        WebElement passwordField = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.id("login-button"));
-
-        usernameField.sendKeys("standard_user");
-        passwordField.sendKeys("secret_sauce");
-        loginButton.click();
+    public void testLogin()  {
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
 
         WebElement productPageHeader = driver.findElement(By.id("header_container"));
-        Assertions.assertThat(productPageHeader).isNotNull();
+        assertThat(productPageHeader).isNotNull();
     }
 }
